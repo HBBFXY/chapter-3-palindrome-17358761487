61760f933be658bda6e185ddfb494b7d6fd0fd9a
17358761487 <2053752041@qq.com>
2025-09-14 11:56:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# 这里编写你的代码
+
+def main():
+    num_str = input("")
+    if len(num_str) != 5 or not num_str.isdigit():
+        print("输入错误: 请输入5位数字")
+        return
+    if num_str == num_str[::-1]:
+        print("是回文数")
+    else:
+        print("不是回文数")
+
+if __name__ == "__main__":
+    main()
